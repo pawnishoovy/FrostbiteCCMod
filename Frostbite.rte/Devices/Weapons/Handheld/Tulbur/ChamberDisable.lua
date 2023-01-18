e846f1aa22cd0f203d07088d347ffd058a2b1f2f
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2023-01-18 23:38:53 +0200

    new shotgun, misc stuff

---
+++ Frostbite.rte/Devices/Weapons/Handheld/Tulbur/ChamberDisable.lua
@@ -0,0 +1,12 @@
+function OnDetach(self)
+
+	self:DisableScript("Frostbite.rte/Devices/Weapons/Handheld/Tulbur/Chamber.lua");
+	self.ReloadTime = 9999;
+	
+end
+
+function OnAttach(self)
+
+	self:EnableScript("Frostbite.rte/Devices/Weapons/Handheld/Tulbur/Chamber.lua");
+	
+end
